8f821cb51f6ce7c322a88bd60a0524bfe027803e
Garima199221 <100105977+Garima199221@users.noreply.github.com>
2022-03-10 09:11:25 +1300

    Update CalculatorDemo.java

--- CalculatorDemo.java
+++ CalculatorDemo.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class CalculatorDemo {
    public static void main(String[] args) {
-  
+  System.out.println("Showing addition ");
 		int a = 12;
 		int b = 17;
 		int c = 342;
