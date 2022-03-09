69196f392e8786c308f18d45e472b0bd1286bc6d
Garima199221 <100105977+Garima199221@users.noreply.github.com>
2022-03-09 18:05:39 +1300

    Add files via upload

---
+++ CalculatorDemo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Calculator {
+   public static void main(String[] args) {
+  
+		int a = 12;
+		int b = 17;
+		int c = 342;
+		int d = a + b + c;
+		System.out.println("Addition result = " + d);
+	     
+	   }
+	}
+
